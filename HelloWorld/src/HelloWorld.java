8c59eab33cc9a8f166df82fe9a28294f0f1e8cf5
Giraffe <Giraffe@Giraffe-PC>
2013-08-26 21:10:57 -0400

    added star decrease thingy

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,7 +1,19 @@
+import javax.swing.JOptionPane;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        int intC = 15;
+        String strOut = "";
+        while(intC>0){
+        	strOut = "";
+        	for(int i=0;i<intC;i++){
+        		strOut = strOut + "*";
+        	}
+        	System.out.println(strOut);        	
+        	intC--;
+        }
     }
 
 }
